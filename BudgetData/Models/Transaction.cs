96525a72c7ac776ffc3086479f6ce77397738882
pmorenoum <pablo.morenoum@andrena.de>
2022-06-02 09:07:23 +0200

    adjust data model and install Entity Framework and SQL server in production and test project

--- BudgetData/Models/Transaction.cs
+++ BudgetData/Models/Transaction.cs
@@ -2,8 +2,14 @@ using System.Numerics;
 
 namespace BudgetData.Models;
 
-public record Transaction(DateOnly DateOfTransaction, string DescriptionOfTransaction, BigInteger ValueOfTransaction)
+public class Transaction
 {
+    public int Id { get; set; }
+    public DateTime DateOfTransaction { get; set; }
+    public string DescriptionOfTransaction { get; set; }
+    public decimal ValueOfTransaction { get; set; }
+    public string Budget { get; set; }
+
     public override string ToString()
     {
         return $"{DateOfTransaction:dd.MM.yyyy}\t {DescriptionOfTransaction}\t {ValueOfTransaction:C}";
