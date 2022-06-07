c6b1a4e592b3e4fffe4dbce5b3ef1d34f8d2c645
Stefan.Fimpel <stefan.fimpel@andrena.de>
2022-06-07 10:17:13 +0200

    Fix test for Transaction.ToString()

--- BudgetDataTest/Models/TransactionTest.cs
+++ BudgetDataTest/Models/TransactionTest.cs
@@ -8,11 +8,18 @@ namespace BudgetDataTest;
 
 public class TransactionTest
 {
-    // [Fact]
-    // public void TransactionToString_shouldReturnFormattedString()
-    // {
-    //     Transaction testTransaction = new(1, new DateOnly(2022,12,12), "Ein Test", new BigInteger(3));
-    //
-    //     testTransaction.ToString().Should().Be("12.12.2022\t Ein Test\t 3,00 €");
-    // }
+    [Fact]
+    public void TransactionToString_shouldReturnFormattedString()
+    {
+        Transaction testTransaction = new()
+        {
+            Id = 1,
+            DateOfTransaction = new DateTime(2022,12,12),
+            DescriptionOfTransaction = "Ein Test",
+            Budget = "Testbudget",
+            ValueOfTransaction = 3
+        };
+    
+        testTransaction.ToString().Should().Be("12.12.2022\t Ein Test\t 3,00 €");
+    }
  }
