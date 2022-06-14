687887ad762beae5549f2b96288549afb642c581
pablo.morenoum <pablo.morenoum@andrena.de>
2022-06-14 18:43:36 +0200

    #10 make tables hoverable, put create link on top line, change privacy page

--- BudgetData/Controllers/TransactionController.cs
+++ BudgetData/Controllers/TransactionController.cs
@@ -20,7 +20,6 @@ namespace BudgetData.Controllers
         // GET: Transaction
         public async Task<IActionResult> Index(string budgetFilter, string searchString)
         {
-            
             _transactionService.SearchTransactionsByDescription(searchString);
             _transactionService.FilterBudgets(budgetFilter);
             
--- BudgetDataTest/Controllers/KontoServiceControllerTest.cs
+++ BudgetDataTest/Controllers/KontoServiceControllerTest.cs
@@ -26,13 +26,13 @@ public class KontoServiceControllerTest: IDisposable
 
         using (var context = new BudgetDataContext(Options))
         {
-            context.Transaction!.Add(new Transaction()
+            context.Transaction!.Add(new Transaction
                 { DescriptionOfTransaction = "abc", Budget = "Budget1", ValueOfTransaction = (decimal)0.01 });
-            context.Transaction.Add(new Transaction()
+            context.Transaction.Add(new Transaction
                 { DescriptionOfTransaction = "bcd", Budget = "Budget1", ValueOfTransaction = (decimal)0.10 });
-            context.Transaction.Add(new Transaction()
+            context.Transaction.Add(new Transaction
                 { DescriptionOfTransaction = "cde", Budget = "Budget2", ValueOfTransaction = (decimal)1.00 });
-            context.Transaction.Add(new Transaction()
+            context.Transaction.Add(new Transaction
                 { DescriptionOfTransaction = "xyz", Budget = "Hifi", ValueOfTransaction = (decimal)10.00 });
             context.SaveChanges();
         }
