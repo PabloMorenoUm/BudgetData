4f306cd2bd59d3778c22ccf08e419d25f6532222
pablo.morenoum <pablo.morenoum@andrena.de>
2022-06-15 17:30:54 +0200

    #18 beautify budget page

--- BudgetData/Models/Budget.cs
+++ BudgetData/Models/Budget.cs
@@ -7,7 +7,7 @@ public class Budget
     public int Id { get; set; }
 
     [Required]
-    [Display(Name = "Budget")]
+    [Display(Name = "Kategorie")]
     public string Purpose { get; set; }
     
     [DataType(DataType.Currency)]
